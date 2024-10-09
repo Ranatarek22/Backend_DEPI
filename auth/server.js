17bb9bcf82a2408fc9b562980c603717e2740026
Ranatarek22 <rt589611@gmail.com>
2024-10-09 12:28:29 +0300

    confg

--- auth/server.js
+++ auth/server.js
@@ -23,7 +23,9 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
-
+app.get("/", (req, res) => {
+  res.send("Hello from Express with MongoDB Atlas!");
+});
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
